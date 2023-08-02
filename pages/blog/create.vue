11de3031f31874b5656263f5c486a8f15ee6074d
Dushyant Goel <dushyant95goel@gmail.com>
2023-08-02 17:38:28 +0530

    some ui and forms

---
+++ pages/blog/create.vue
@@ -0,0 +1,21 @@
+<template>
+    <lable>Title</lable>
+    <input v-model="titleRef" type="text">
+    <lable>Content</lable>
+    <input v-model="contentRef" type="textarea">
+    <button @click="handleBlogSubmit">Post</button>
+</template>
+
+<script setup lang="ts">
+
+const titleRef = ref("");
+const contentRef = ref("");
+
+function handleBlogSubmit() {
+    const title: string = titleRef.value;
+    const content: string = contentRef.value;
+
+    
+}
+
+</script>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,4 +1,24 @@
 <template>
-    <h1>Login Page</h1>
+  <h1>Login</h1>
+
+  <label>Username</label>
+  <input v-model="usernameRef" type="text" placeholder="username">
+  <label>Password</label>
+  <input v-model="passwordRef" type="password">
+  <button @click="handleLogIn">Login</button>
+
 </template>
 
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let usernameRef = ref("")
+let passwordRef = ref("")
+
+function handleLogIn() {
+  const username: string = usernameRef.value;
+  const password: string = passwordRef.value;
+
+  GqlSignIn({username, password});
+}
+</script>
